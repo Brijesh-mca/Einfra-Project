f5f0496c70498055f3f7d753465493e02de18d05
Brijesh-mca <ibrijesh.mca@gmail.com>
2025-02-11 13:02:55 +0530

    adding and changes in some files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//Loader 
+
+// Simulate a loading delay
+window.onload = function() {
+    // setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    // }, 500); // 
+};
+
+// setTimeout(() => {
+//     document.getElementById("msg").style.display = "none"
+// }, 2000);
