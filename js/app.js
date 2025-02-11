054b8a9528672573d82c6b3df54c79450ab52764
Brijesh-mca <ibrijesh.mca@gmail.com>
2025-02-11 13:53:59 +0530

    adding gsap animation

--- js/app.js
+++ js/app.js
@@ -11,3 +11,12 @@ window.onload = function() {
 // setTimeout(() => {
 //     document.getElementById("msg").style.display = "none"
 // }, 2000);
+
+
+//Animation with Gsap 
+
+
+
+var tl = gsap.timeline();
+tl.from("#myNav", {y: -100, duration: 0.5,delay:0.5 });
+tl.from(".nav-item", {y: -100, duration: 0.5, opacity:0, stagger:0.2 , scale:1.5});
